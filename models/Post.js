7e4991f6001fb3be107b57e14ca6ae86a61a29ae
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-10-04 21:11:20 -0500

    Created vote routes and set static model

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,30 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Post model
-class Post extends Model {}
+class Post extends Model {
+    static upvote(body, models){ // model methods? what's is difference from instance methods??
+        return models.Vote.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ]
+            });
+        });
+    }
+}
 
 // create fields/column for Post model
 Post.init(
