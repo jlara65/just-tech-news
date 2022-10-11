2b8f25da59e7ac937ecda6f31a7d5754ded56c69
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-10-10 23:55:20 -0500

    created upvote and comments into front end

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,18 +13,22 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+    // check the session
+    if (req.session) {
     // expects => {comment_text: "this is comment", user_id: 1, post_id:2}
     Comment.create({
         comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
+        post_id: req.body.post_id,
+        // use the id from session
+        user_id: req.session.user_id
+        
     })
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
         console.log(err);
         res.status(400).json(err);
     });
-
+}
 });
 
 router.delete('/:id', (req, res) => {
